c0e055f8c736d1d8ff467a4a552679da33cf12bc
Cooper <Cooperransom08@gmail.com>
2024-08-29 02:28:46 +0000

    init

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,6 @@
 import { Footer } from "@/components/layout/footer";
 import { Navbar } from "@/components/layout/navbar";
-import { Sidebar } from "@/components/layout/sidebar";
 import { cn } from "@/lib/utils";
-import { useLayoutStore } from "@/stores/layout";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
@@ -11,18 +9,15 @@ export const Route = createRootRoute({
 });
 
 function Root() {
-  const sidebar = useLayoutStore((state) => state.sidebar);
-
   return (
     <>
-      <Sidebar />
-      <main className={cn("flex min-h-[calc(100vh_-_56px)] flex-col transition-[margin-left] duration-300 ease-in-out dark:bg-zinc-900", sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72")}>
+      <main className={cn("flex min-h-[calc(100vh_-_56px)] flex-col transition-[margin-left] duration-300 ease-in-out dark:bg-zinc-900")}>
         <Navbar />
         <div className="flex flex-1 p-8 [&>*]:w-full">
           <Outlet />
         </div>
       </main>
-      <footer className={cn("transition-[margin-left] duration-300 ease-in-out", sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72")}>
+      <footer className={cn("transition-[margin-left] duration-300 ease-in-out")}>
         <Footer />
       </footer>
       <TanStackRouterDevtools />
