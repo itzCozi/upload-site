8a60c806d6c7d603378b9ee94cde76dffcfa00fe
Cooper <Cooperransom08@gmail.com>
2024-09-02 22:05:48 +0000

    replace flies with files

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -16,8 +16,8 @@ export function Navbar() {
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary sticky top-0 z-10 w-full shadow backdrop-blur">
       <div className="mx-4 flex h-14 items-center justify-between sm:mx-8">
         <Link to="/" className="flex flex-row gap-1 items-center">
-          <img src="src/assets/icon.png" alt="flies.vc icon" className="h-10" />
-          <h1 className="font-bold text-3xl">flies.vc</h1>
+          <img src="src/assets/icon.png" alt="files.vc icon" className="h-10" />
+          <h1 className="font-bold text-3xl">files.vc</h1>
         </Link>
         <div className="flex flex-1 items-center justify-end space-x-2">
           <ThemeToggle />
